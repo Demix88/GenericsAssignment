ef17368b60f77eb986cc46f679d389aca2ec5c94
Demix88 <skrasnauskis@gmail.com>
2022-04-05 22:04:26 +0100

    Rewrote entire code

--- GenericsAssignment/Program.cs
+++ GenericsAssignment/Program.cs
@@ -6,46 +6,48 @@ using System.Threading.Tasks;
 
 namespace GenericsAssignment
 {
-
-
-        public class Employee
-        {
-            public int Id { get; set; }
-            public string FirstName { get; set; }
-            public string LastName { get; set; }
-        class Program
+    class Program
+    {
+        public class Employee<T>
         {
 
+            public List<T> Things { get; set; }
 
 
 
-            public static void Main()
+        }
+        public static void Main(string[] args)
+        {
+
+            Employee<string> emp1 = new Employee<string>();
+            Employee<int> emp2 = new Employee<int>();
+            emp1.Things = new List<string>();
+            emp1.Things.Add("Chris");
+            emp1.Things.Add("Bob");
+            emp1.Things.Add("Richard");
+            emp1.Things.Add("Natalia");
+            emp1.Things.Add("Rebekah");
+
+            emp2.Things = new List<int>();
+            emp2.Things.Add(007);
+            emp2.Things.Add(003);
+            emp2.Things.Add(695);
+            emp2.Things.Add(027);
+            emp2.Things.Add(197);
+
+            for (int i = 0; i < emp1.Things.Count; i++)
             {
-                List<string> Things = new List<string>() { "a", "b", "c", "d" };
-                Console.WriteLine("An Employee in position 0 :" + Things[0]);
-                Console.WriteLine("An Employee in position 3 :" + Things[3]);
-                List<int> things = new List<int>() { 1, 3, 7, 9 };
-                Console.WriteLine("An Employee in position 0 :" + things[0]);
-                Console.WriteLine("An Employee in position 3 :" + things[3]);
-                
-
-                List<Employee> employees = new List<Employee>()
-                {
-                    new Employee {Id= 1, FirstName="Jessica", LastName="Ring"},
-                    new Employee {Id= 5, FirstName="Stefan", LastName="King"},
-                    new Employee {Id= 74, FirstName="Jeremy", LastName="Iron"},
-                    new Employee {Id= 74, FirstName="Jayden", LastName="Storm"},
-                };
-                for (int i = 0; i < employees.Count; i++)
-                {
-                    Console.WriteLine("Id:{0}, First Name :{1} Last Name:{2}",employees[i].Id,employees[i].FirstName,employees[i].LastName);
-                }
-                Console.ReadLine();
-
-
+                Console.WriteLine("Employee Name :{0} ", emp1.Things[i]);
 
             }
-        }
+            for (int i = 0; i < emp2.Things.Count; i++)
+            {
+                Console.WriteLine(" Employee ID :{0} ", emp2.Things[i]);
+            }
+            Console.ReadLine();
 
+
+        }
     }
 }
+
